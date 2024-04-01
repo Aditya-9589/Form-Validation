fbd3d938d80a68c4b412ef1f5356838c113ef5f9
Aditya Hariharno <adityahariharno12@gmail.com>
2024-04-01 21:39:54 +0530

    .

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const Cpassword = document.getElementById("confirm-password");
+const submit = document.getElementById("btn");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    validateInputs();
+})
+
+const validateinputs = () => {
+    
+}
