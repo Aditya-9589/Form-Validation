034466ff9372c781fce6a7bd220de74a39736400
Aditya Hariharno <adityahariharno12@gmail.com>
2024-04-02 16:04:16 +0530

    Last Commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const Cpassword = document.getElementById("confirm-password");
+const cPassword = document.getElementById("confirm-password");
 const submit = document.getElementById("btn");
 
 form.addEventListener('submit', (e) => {
@@ -15,13 +15,47 @@ const validateInputs = () => {
     const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
-    const cpasswordValue = Cpassword.value.trim();
+    const cPasswordValue = cPassword.value.trim();
 
+    // ----  UserName :-  ---- 
     if(usernameValue === '' ) {
-        setError(username, 'Username is required');
+        setError(username, 'Username is required.');
     }
     else {
         setSuccess(username);
+    }
+
+    //----  E-mail :-  ----
+    if(emailValue === '' ) {
+        setError(email, " Email is required.")
+    }
+    else if(!isValidEmail(emailValue)) {
+        setError(email, "Provide a valid Email Address.")
+    }
+    else {
+        setSuccess(email);
+    }
+
+    // ----  Password :-  ---- 
+    if(passwordValue === '' ) {
+            setError(password, 'Password is required.');
+        }
+    else if (passwordValue.length < 8) {
+        setError(password, "Password must be at least 8 characters.")
+    }
+    else {
+        setSuccess(password);
+    }
+
+    // ----  Confirm Password :-  ----
+    if(cPasswordValue === '' ) {
+        setError(cPassword, "Please confirm your Password.");
+    }
+    else if (cPasswordValue !== passwordValue) {
+        setError(cPassword, "Password doesn't match.");
+    }
+    else {
+        setSuccess(cPassword);
     }
 }
 
@@ -29,6 +63,22 @@ const setError = (element, message) => {
     const inputControl = element.parentElement;
     const errorDisplay = inputControl.querySelector('.error');
     errorDisplay.innerText = message;
+
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+
 }
 
-console.log("Hello Aditya...");
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
